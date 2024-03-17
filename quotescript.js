1fafeb0f2943c20506105acf7c4a5f9506407c95
avs920x <128978289+avs920x@users.noreply.github.com>
2024-03-17 10:33:53 -0400

    Add files via upload

---
+++ quotescript.js
@@ -0,0 +1,49 @@
+//define constant variables
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+let apiQuotes = [];
+
+
+//show new quote
+function newQuote() {
+    //pick random quote from api array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    authorText.textContent = quote.author;
+    quoteText.textContent = quote.text;
+    if(!quote.author) {
+        authorText.textContent = "Unknown";
+    } else {
+        authorText.textContent = quote.author;
+    }
+}
+
+
+//get quotes via API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        //catch error here
+    }
+}
+
+//on load
+getQuotes();
+
+/* Set the width of the side navigation to 250px */
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
